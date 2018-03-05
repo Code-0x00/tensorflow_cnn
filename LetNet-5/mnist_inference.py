4e365ea9e4948bb11656afd4cafcac53489158ec
Code-X <yohoho233@gmail.com>
2018-03-05 19:02:44 +0800

    combine layers

--- LetNet-5/mnist_inference.py
+++ LetNet-5/mnist_inference.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 
-input_node=784
-output_node=10
-layer1_node=500
-print input_node
+xNode=[784,500,10]
+xLayer=['layer1','layer2']
+
+input_node=xNode[0]
+output_node=xNode[-1]
+
 def get_weight_variable(shape,regularizer):
 	weights=tf.get_variable("weights",shape,initializer=tf.truncated_normal_initializer(stddev=0.1))
 	if regularizer!=None:
@@ -11,12 +13,10 @@ def get_weight_variable(shape,regularizer):
 	return weights
 
 def inference(input_tensor,regularizer):
-	with tf.variable_scope('layer1'):
-		weights=get_weight_variable([input_node,layer1_node],regularizer)
-		biases=tf.get_variable("biases",[layer1_node],initializer=tf.constant_initializer(0.0))
-		layer1=tf.nn.relu(tf.matmul(input_tensor,weights)+biases)
-	with tf.variable_scope('layer2'):
-		weights=get_weight_variable([layer1_node,output_node],regularizer)
-		biases=tf.get_variable("biases",[output_node],initializer=tf.constant_initializer(0.0))
-		layer2=tf.nn.relu(tf.matmul(layer1,weights)+biases)
-	return layer2
+	layer_out=input_tensor
+	for i in range(len(xLayer)):
+		with tf.variable_scope(xLayer[i]):
+			weights=get_weight_variable([xNode[i],xNode[i+1]],regularizer)
+			biases=tf.get_variable("biases",[xNode[i+1]],initializer=tf.constant_initializer(0.0))
+			layer_out=tf.nn.relu(tf.matmul(layer_out,weights)+biases)
+	return layer_out
