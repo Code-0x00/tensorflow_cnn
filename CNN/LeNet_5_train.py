e8a4c3683bbbbd3eee23206e291879dfcc0dcf56
Code-X <yohoho233@gmail.com>
2018-03-15 21:00:15 +0800

    reWrite

--- CNN/LeNet_5_train.py
+++ CNN/LeNet_5_train.py
@@ -6,6 +6,23 @@ import cnn
 
 import numpy as np
 
+#data_size:w,h,channel
+#conv_size:k_size,k_size,channel,deep
+#pool_size:channel,k_size.k_size,deep
+#fcon_size:input,output
+
+lenet=[
+{"name":"l0input","type":"data","size":[28,28,1]},
+{"name":"l1conv1","type":"conv","size":[ 5, 5, 1, 6],"strides":[1,1,1,1],"padding":"SAME"},
+{"name":"l2pool1","type":"pool","size":[ 1, 2, 2, 1],"strides":[1,2,2,1],"padding":"SAME"},
+{"name":"l3conv2","type":"conv","size":[ 5, 5, 6,16],"strides":[1,1,1,1],"padding":"VALID"},
+{"name":"l4pool2","type":"pool","size":[ 1, 2, 2, 1],"strides":[1,2,2,1],"padding":"SAME"},
+{"name":"l5conv3","type":"conv","size":[ 5,5,16,120],"strides":[1,1,1,1],"padding":"VALID"},
+{"name":"reshape","type":"shape"},
+{"name":"l6fcon1","type":"fcon","size":[120,84]},
+{"name":"l7fcon2","type":"fcon","size":[84,10]}
+]
+
 batch_size=100
 learning_rate_base=0.2
 learning_rate_decay=0.99
@@ -17,16 +34,18 @@ model_save_path='./model/'
 model_name="model.ckpt"
 
 def train(mnist):
-	x=tf.placeholder(tf.float32,[
-		batch_size,
-		cnn.image_size,
-		cnn.image_size,
-		cnn.num_channels],
-		name='x-input')
-	y_=tf.placeholder(tf.float32,[None,cnn.output_node],name='y-input')
+	x=tf.placeholder(tf.float32,
+					[
+					batch_size,
+					lenet[0]['size'][0],
+					lenet[0]['size'][1],
+					lenet[0]['size'][2]
+					],
+					name='x-input')
+	y_=tf.placeholder(tf.float32,[None,lenet[-1]['size'][1]],name='y-input')
 
 	regularizer=tf.contrib.layers.l2_regularizer(regularization_rate)
-	y=cnn.inference(x,regularizer,train=True)
+	y=cnn.inference(lenet,x,regularizer,train=True)
 
 	global_step=tf.Variable(0,trainable=False)
 	variable_averages=tf.train.ExponentialMovingAverage(moving_average_decay,global_step)
@@ -51,9 +70,9 @@ def train(mnist):
 		for i in range(training_steps):
 			xs,ys=mnist.train.next_batch(batch_size)
 			reshaped_xs=np.reshape(xs,(batch_size,
-				cnn.image_size,
-				cnn.image_size,
-				cnn.num_channels))
+					lenet[0]['size'][0],
+					lenet[0]['size'][1],
+					lenet[0]['size'][2]))
 			_None,loss_value,step=sess.run([train_op,loss,global_step],feed_dict={x:reshaped_xs,y_:ys})
 
 			if i%1000==0:
--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,43 +1,19 @@
 import tensorflow as tf
 
-xNode=[784,500,10]
-xLayer=['layer1','layer2']
+def inference(cnnNet,input_tensor,regularizer,train):
 
-input_node=xNode[0]
-output_node=xNode[-1]
+	for layer in cnnNet:
+		if layer['type']=='data':
+			last_layer_output=input_tensor
 
-image_size=28
-num_channels=1
-num_labels=10
-
-conv1_deep=32
-conv1_size=5
-
-conv2_deep=64
-conv2_size=5
-
-fc_size=512
-lenet=[
-{"name":"l1conv1","type":"conv","size":[ 5, 5, 1, 6],"strides":[1,1,1,1],"padding":"SAME"},#k_size,k_size,channel,deep
-{"name":"l2pool1","type":"pool","size":[ 1, 2, 2, 1],"strides":[1,2,2,1],"padding":"SAME"},
-{"name":"l3conv2","type":"conv","size":[ 5, 5, 6,16],"strides":[1,1,1,1],"padding":"VALID"},
-{"name":"l4pool2","type":"pool","size":[ 1, 2, 2, 1],"strides":[1,2,2,1],"padding":"SAME"},
-{"name":"l5conv3","type":"conv","size":[ 5,5,16,120],"strides":[1,1,1,1],"padding":"VALID"},
-{"name":"reshape","type":"shape"},
-{"name":"l6fcon1","type":"fcon","size":[120,84]},
-{"name":"l7fcon2","type":"fcon","size":[84,10]}
-]
-def inference(input_tensor,regularizer,train):
-	last_layer_output=input_tensor
-	print 'l0data',last_layer_output.shape
-	for layer in lenet:
-		if layer["type"]=="conv":
+		elif layer["type"]=="conv":
 			with tf.variable_scope(layer['name']):
 				weights=tf.get_variable("weight",layer['size'],initializer=tf.truncated_normal_initializer(stddev=0.1))
 				biases=tf.get_variable("bias",layer['size'][3],initializer=tf.constant_initializer(0.0))
 				conv=tf.nn.conv2d(last_layer_output,weights,strides=layer['strides'],padding=layer['padding'])
 				relu=tf.nn.relu(tf.nn.bias_add(conv,biases))
 				last_layer_output=relu
+
 		elif layer['type']=='fcon':
 			with tf.variable_scope(layer['name']):
 				weights=tf.get_variable('weight',layer['size'],initializer=tf.truncated_normal_initializer(stddev=0.1))
@@ -46,10 +22,12 @@ def inference(input_tensor,regularizer,train):
 				biases=tf.get_variable('bias',layer['size'][1],initializer=tf.constant_initializer(0.1))
 				relu=tf.nn.relu(tf.matmul(last_layer_output,weights)+biases)
 				last_layer_output=relu
+
 		elif layer['type']=='pool':
 			with tf.name_scope(layer['name']):
 				pool=tf.nn.max_pool(last_layer_output,ksize=layer['size'],strides=layer['strides'],padding=layer['padding'])
 				last_layer_output=pool
+
 		elif layer['type']=='shape':
 			pool_shape=last_layer_output.get_shape().as_list()
 			nodes=pool_shape[1]*pool_shape[2]*pool_shape[3]
